c602bae8f1266d80da96dd0d347d1f12cb54b852
Ryon Hunter <ryon@websolutionsnyc.com>
2017-01-26 16:13:23 -0500

    Add assertException

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,5 +2,15 @@
 
 class TestCase extends \PHPUnit_Framework_TestCase
 {
-
+    protected function assertException( $test, $callback )
+    {
+        try {
+            $test();
+            $this->fail('Exception not thrown');
+        } catch( \Throwable $e ) {
+            $callback($e);
+        } catch( \Exception $e ) {
+            $callback($e);
+        }
+    }
 }
